0d14e518b841b6e1c0f218913a1e2dd3a2851b38
Raghav-28 <kraghavgupta@gmail.com>
2025-02-09 15:27:44 +0530

    Sidebars UI completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,45 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+
+    const loggedIn = {firstName:'Raghav', lastName:'Gupta', email:'graghav286@gmail.com'}
+
   return (
-    <div>
-      Home
+    <section className="home">
+    <div className="home-content">
+      <header className="home-header">
+        Welcome Raghav
+        <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+        />
+
+        <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.25}
+        />
+      </header>
+
+      {/* <RecentTransactions 
+        accounts={accountsData}
+        transactions={account?.transactions}
+        appwriteItemId={appwriteItemId}
+        page={currentPage}
+      /> */}
     </div>
+
+    <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance:123.55},{currentBalance:8542.33}]}
+    />
+  </section>
   )
 }
 
