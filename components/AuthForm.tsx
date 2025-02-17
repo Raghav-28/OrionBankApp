913d9224d6d1a76f16719be074b953613feecfe6
Raghav-28 <kraghavgupta@gmail.com>
2025-02-17 18:30:35 +0530

    Updated Dependencies

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,7 +13,7 @@ import CustomInput from "./CustomInput";
 import { authformSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from 'next/navigation';
-import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 import PlaidLink from "./PlaidLink";
 
 
